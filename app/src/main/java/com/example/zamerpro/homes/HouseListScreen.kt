e95d1e2e94e82deccbf6f1b20fee53e7a99edde0
maks9678 <115086005+maks9678@users.noreply.github.com>
2025-10-17 00:11:16 +0300

    создание экрана материалы

--- app/src/main/java/com/example/zamerpro/homes/HouseListScreen.kt
+++ app/src/main/java/com/example/zamerpro/homes/HouseListScreen.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -27,7 +31,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,13 +38,18 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.zamerpro.Class.House
 import com.example.zamerpro.home.HOUSE_SCREEN_ROUTE
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import org.intellij.lang.annotations.JdkConstants
 
 const val HOUSES_LIST_SCREEN_ROUTE = "housesListScreen"
 
@@ -135,7 +143,7 @@ fun HousesListScreen(
         showDialog = showDialog,
         newHouseName = newHouseName,
         onNewHouseNameChange = { newHouseName = it },
-        onShowDialogChange = { viewModel.onShowDialogChange(it)},
+        onShowDialogChange = { viewModel.onShowDialogChange(it) },
         onConfirmNewHouse = {
             if (newHouseName.isNotBlank()) {
                 viewModel.createNewHouse(newHouseName) { houseId ->
@@ -164,29 +172,42 @@ fun HousesListScreenInternal(
     onDeleteHouse: (House) -> Unit,
     onHouseClick: (House) -> Unit,
 ) {
-        Box(
-            // ✅ Применяем отступы, чтобы контент не уезжал под панели
-            modifier = modifier.fillMaxSize()
+    Scaffold(
+        modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text("Объекты") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             // Контент экрана (список домов или текст)
             if (houses.isEmpty()) {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text(
-                        modifier = Modifier.padding(horizontal = 8.dp),
-                        textAlign = TextAlign.Center,
-                        text = "У вас пока нет объектов. Нажмите '+' для создания."
-                    )
-                }
+                Text(
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    textAlign = TextAlign.Center,
+                    text = "У вас пока нет объектов. Нажмите '+' для создания."
+                )
             } else {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
-                    contentPadding = PaddingValues(
-                        start = 16.dp,
-                        end = 16.dp,
-                        bottom = 80.dp
-                    ), // Добавляем отступ снизу, чтобы FAB не перекрывал последний элемент
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    contentPadding = PaddingValues(8.dp), // Добавляем отступ снизу, чтобы FAB не перекрывал последний элемент
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                )
+                {
                     items(houses, key = { it.id }) { house ->
                         HouseListItem(
                             house = house,
@@ -196,30 +217,32 @@ fun HousesListScreenInternal(
                     }
                 }
             }
+        }
 
-            // Диалог создания остается здесь, он будет показан поверх всего
-            if (showDialog) {
-                AlertDialog(
-                    onDismissRequest = { onShowDialogChange(false) },
-                    title = { Text("Новый объект") },
-                    text = {
-                        OutlinedTextField(
-                            value = newHouseName,
-                            onValueChange = onNewHouseNameChange,
-                            label = { Text("Название объекта") },
-                            singleLine = true
-                        )
-                    },
-                    confirmButton = {
-                        Button(onClick = onConfirmNewHouse) { Text("Создать") }
-                    },
-                    dismissButton = {
-                        Button(onClick = { onShowDialogChange(false) }) { Text("Отмена") }
-                    }
-                )
-            }
+        // Диалог создания остается здесь, он будет показан поверх всего
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { onShowDialogChange(false) },
+                title = { Text("Новый объект") },
+                text = {
+                    OutlinedTextField(
+                        value = newHouseName,
+                        onValueChange = onNewHouseNameChange,
+                        label = { Text("Название объекта") },
+                        singleLine = true
+                    )
+                },
+                confirmButton = {
+                    Button(onClick = onConfirmNewHouse) { Text("Создать") }
+                },
+                dismissButton = {
+                    Button(onClick = { onShowDialogChange(false) }) { Text("Отмена") }
+                }
+            )
         }
     }
+}
+
 
 @Composable
 fun HouseListItem(
@@ -231,29 +254,40 @@ fun HouseListItem(
     Card(
         modifier = modifier
             .fillMaxWidth()
+            .height(130.dp)
             .clickable(onClick = onClick),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        shape = MaterialTheme.shapes.medium,
+        // 3. Можно задать цвета, если нужно (по умолчанию они из темы)
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant, // Чуть другой фон
+        )
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .padding(16.dp)
+                .padding(8.dp)
                 .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+
         ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(house.name, style = MaterialTheme.typography.titleMedium)
+            Text(house.name,
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 2,
+                textAlign = TextAlign.Center)
+            Row (modifier = Modifier,
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically) {
                 Text(
                     "Изменен: ${formatTimestamp(house.lastModified)}",
                     style = MaterialTheme.typography.bodySmall
                 )
-            }
-            IconButton(onClick = onDelete) {
-                Icon(
-                    Icons.Filled.Delete,
-                    contentDescription = "Удалить объект",
-                    tint = MaterialTheme.colorScheme.error
-                )
+
+                IconButton(onClick = onDelete) {
+                    Icon(
+                        Icons.Filled.Delete,
+                        contentDescription = "Удалить объект",
+                        tint = MaterialTheme.colorScheme.error
+                    )
+                }
             }
         }
     }
